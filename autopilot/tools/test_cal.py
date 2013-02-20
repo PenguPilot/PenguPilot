8a23cebd60bac27a7fd6e6ce80696f55dab5be62
Tobias Simon <tobias.simon@tu-ilmenau.de>
2013-02-20 20:32:37 +0100

    added calibration code

---
+++ autopilot/tools/test_cal.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+
+ Computes calibration from stdin and writes aligned measurements to stdout
+
+ Copyright (C) 2013 Tobias Simon, Ilmenau University of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+if __name__ == '__main__':
+   from sys import stdin
+   from cal_lib import Calibration
+   lines = stdin.readlines()
+   array = []
+   for line in lines:
+      array.append(map(float, line.split(' ')))
+   cal = Calibration(array)
+   for entry in array:
+      print cal.apply(entry)
---
+++ autopilot/tools/txt_col.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+
+ Filter Human-readable Logfile based on column Names
+
+ Copyright (C) 2012 Tobias Simon, Ilmenau University of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+if __name__ == '__main__':
+   from sys import stdin, argv
+   extract = argv[1:]
+   lines = stdin.readlines()
+   header = lines[0].split(' ')
+   indices = []
+   for name in extract:
+      indices.append(header.index(name))
+   for line in lines[1:]:
+      array = []
+      line_array = line.split(' ')
+      for index in indices:
+         array.append(line_array[index])
+      print ' '.join(array)
+
