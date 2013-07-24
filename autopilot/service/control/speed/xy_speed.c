86ba7328a5e1f6ec6aa6157aca3c8848955acba7
Tobias Simon <tobias.simon@tu-ilmenau.de>
2013-07-24 17:21:36 +0200

    minor changes

--- autopilot/service/control/speed/xy_speed.c
+++ autopilot/service/control/speed/xy_speed.c
@@ -30,60 +30,11 @@
 #include <util.h>
 #include <threadsafe_types.h>
 #include <opcd_interface.h>
-#include <meschach/matrix.h>
 
 #include "../../util/math/conv.h"
+#include "../../util/math/vec2.h"
 
 
-typedef struct
-{
-   MAT *matrix; /* rotation matrix */
-   VEC *in; /* rotation input vector */
-   VEC *out; /* rotation output vector */
-}
-rot2_t;
-
-
-static void rot2_init(rot2_t *rot2)
-{
-   rot2->matrix = m_get(2, 2);
-   ASSERT_NOT_NULL(rot2->matrix);
-   rot2->in = v_get(2);
-   ASSERT_NOT_NULL(rot2->in);
-   rot2->out = v_get(2);
-   ASSERT_NOT_NULL(rot2->out);
-}
-
-
-static void rot2_calc(rot2_t *rot2, vec2_t *out, vec2_t *in, float angle)
-{
-   ASSERT_NOT_NULL(rot2->matrix);
-   ASSERT_NOT_NULL(rot2->in);
-   ASSERT_NOT_NULL(rot2->out);
-
-   /* copy arguments to input vector: */
-   rot2->in->ve[0] = in->x;
-   rot2->in->ve[1] = in->y;
-
-   /*
-    * build rotation matrix:
-    *
-    * | cos(x) -sin(x) |
-    * | sin(x)  cos(x) |
-    */
-   rot2->matrix->me[0][0] =  cosf(angle);
-   rot2->matrix->me[0][1] =  sinf(angle);
-   rot2->matrix->me[1][0] = -sinf(angle);
-   rot2->matrix->me[1][1] =  cosf(angle);
-
-   /* perform rotation and copy result into output: */
-   vm_mlt(rot2->matrix, rot2->in, rot2->out);
-   out->x = rot2->out->ve[0];
-   out->y = rot2->out->ve[1];
-}
-
-
-static rot2_t rot2;
 static tsfloat_t p;
 static tsfloat_t angle_max;
 
@@ -98,7 +49,6 @@ void xy_speed_ctrl_init(void)
       OPCD_PARAMS_END
    };
    opcd_params_apply("controllers.xy_speed.", params);
-   rot2_init(&rot2);
 }
 
 
@@ -110,10 +60,12 @@ void xy_speed_ctrl_run(vec2_t *control, vec2_t *speed_setpoint, vec2_t *speed, f
    
    /* calculate 2d speed feedback (angle): */
    vec2_t world_thrust;
-   vec2_scale(&world_thrust, &speed_err, tsfloat_get(&p));
+   vec2_scale(&world_thrust, &speed_err, 1.0/*tsfloat_get(&p)*/);
 
    /* rotate global speed feedback into local control primitives: */
-   rot2_calc(&rot2, control, &world_thrust, yaw);
-   FOR_N(i, 2) control->vec[i] = sym_limit(control->vec[i], deg2rad(tsfloat_get(&angle_max)));
+   vec2_rotate(control, &world_thrust, yaw + M_PI / 2);
+   float a_max = deg2rad(tsfloat_get(&angle_max));
+   control->vec[0] = sym_limit(-control->vec[0], a_max);
+   control->vec[1] = sym_limit(control->vec[1], a_max);
 }
 
