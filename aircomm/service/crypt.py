413a5c5951244baa8696f7fb76707b81a1801c76
Tobias Simon <tobias.simon@tu-ilmenau.de>
2013-12-24 10:55:20 +0100

    added branding and crypto library

---
+++ aircomm/service/crypt.py
@@ -0,0 +1,67 @@
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+  
+ Crypto Service
+
+ Copyright (C) 2009 joonis new media
+ Author: Thimo Kraemer <thimo.kraemer@joonis.de>
+ Copyright (C) 2013 Tobias Simon, Ilmenau University of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+import random, base64
+from hashlib import sha1
+
+
+def crypt(data, key):
+    """RC4 algorithm"""
+    x = 0
+    box = range(256)
+    for i in range(256):
+        x = (x + box[i] + ord(key[i % len(key)])) % 256
+        box[i], box[x] = box[x], box[i]
+    x = y = 0
+    out = []
+    for char in data:
+        x = (x + 1) % 256
+        y = (y + box[x]) % 256
+        box[x], box[y] = box[y], box[x]
+        out.append(chr(ord(char) ^ box[(box[x] + box[y]) % 256]))
+
+    return ''.join(out)
+
+def encrypt(data, key, encode=None, salt_length=4):
+    """RC4 encryption with random salt and final encoding"""
+    salt = ''
+    for n in range(salt_length):
+        salt += chr(random.randrange(256))
+    data = salt + crypt(data, sha1(key + salt).digest())
+    if encode:
+        data = encode(data)
+    return data
+
+def decrypt(data, key, decode=None, salt_length=4):
+    """RC4 decryption of encoded data"""
+    if decode:
+        data = decode(data)
+    salt = data[:salt_length]
+    return crypt(data[salt_length:], sha1(key + salt).digest())
+
