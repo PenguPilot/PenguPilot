450615bc937fbafb3804f646e15c4a305144f081
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-02-27 23:35:49 +0100

    fixed ultra estimator lag

--- autopilot/service/filters/average.c
+++ autopilot/service/filters/average.c
@@ -27,26 +27,19 @@
 #include "average.h"
 
 
-void avg_init(avg_data_t *avg_data, int max)
+void avg_init(avg_data_t *avg_data, float avg)
 {
-   avg_data->avg = 0.0;
-   avg_data->count = 0.0;
-   avg_data->max_count = max;
+   avg_data->avg = avg;
+   avg_data->count = 0;
    avg_data->sum = 0.0;
 }
 
 
-void avg_add(avg_data_t *avg_data, float value)
+float avg_calc(avg_data_t *avg_data, float value)
 {
-   /*
-    * compute average only if
-    * maximum sample count not reached:
-    */
-   if (avg_data->count < avg_data->max_count)
-   {
-      avg_data->sum += value;
-      avg_data->count++;
-      avg_data->avg = avg_data->sum / (float)avg_data->count;
-   }
+   avg_data->sum += value;
+   avg_data->count++;
+   avg_data->avg = avg_data->sum / (float)avg_data->count;
+   return avg_data->avg;
 }
 
