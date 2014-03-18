5e2b98a6d66b4b2de0189ca5f19c970fd5db1260
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-03-18 11:57:54 +0100

    improved arcomm architecture and added kill app stub

---
+++ emerg_kill/service/emerg_kill.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+  
+ Emergency Kill Command Handler
+
+ Copyright (C) 2014 Tobias Simon, Ilmenau University of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+from time import sleep, time
+from scl import generate_map
+from misc import daemonize
+from msgpack import loads
+from aircomm_shared import BCAST, HEARTBEAT
+
+
+def main(name):
+   global socket_map, gps_lock, font, caution_written
+   socket_map = generate_map('aircomm_app')
+   socket = socket_map['in']
+   while True:
+      try:
+         data = loads(socket.recv())
+	 if data[0] == 'EMERGENCY-KILL':
+	    print 'emergency kill requested'
+      except Exception, e:
+         print e
+      sleep(1.0)
+
+
+main('emerg_kill')
+daemonize('emerg_kill', main)
+
