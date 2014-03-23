5d7a7d75c1f13a11e25b0327ed5872171d7c6c9a
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-03-23 20:04:22 +0100

    update of config files

--- heartbeat/service/heartbeat.py
+++ heartbeat/service/heartbeat.py
@@ -37,9 +37,12 @@ from power_pb2 import PowerState
 from msgpack import Packer
 from aircomm_shared import BCAST_NOFW, HEARTBEAT
 
+
 socket_map = None
 voltage = 17.0
 current = 0.4
+critical = False
+
 
 def gps():
    global gps_data, socket_map
@@ -53,6 +56,7 @@ def gps():
             gps_data = GpsData()
             gps_data.ParseFromString(data)
       i += 1
+
 
 def cpuavg():
    global load
@@ -68,9 +72,10 @@ def cpuavg():
 def pmreader():
    s = socket_map['power']
    p = PowerState()
-   global voltage, current
+   global voltage, current, critical
    while True:
       p.ParseFromString(s.recv())
+      critical = p.critical
       if voltage is None:
          voltage = p.voltage
       else:
@@ -81,14 +86,12 @@ def pmreader():
          current = 0.9 * current + 0.1 * p.current
 
 
-
 def mem_used():
    lines = file('/proc/meminfo').readlines()
    total = float(lines[0].split(' ')[-2])
    free = float(lines[1].split(' ')[-2])
    cached = float(lines[3].split(' ')[-2])
    return int(100 * (1.0 - (free + cached) / total))
-
 
 
 def main(name):
@@ -112,7 +115,7 @@ def main(name):
    packer = Packer(use_single_float = True)
    while True:
       try:
-         data = [BCAST_NOFW, HEARTBEAT, int(voltage * 10), int(current * 10), int(load), mem_used()]
+         data = [BCAST_NOFW, HEARTBEAT, int(voltage * 10), int(current * 10), int(load), mem_used(), critical]
          with gps_lock:
             try:
                if gps_data.fix >= 2:
@@ -123,6 +126,7 @@ def main(name):
       except Exception, e:
          pass
       sleep(1.0)
- 
+
+
 daemonize('heartbeat', main)
 
