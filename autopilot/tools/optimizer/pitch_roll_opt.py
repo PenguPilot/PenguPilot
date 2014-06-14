9f48a415d6d60fb794da2e1ec202773a34fcb395
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-06-14 21:03:07 +0200

    added pitch roll control auto-tune

--- autopilot/tools/optimizer/pitch_roll_opt.py
+++ autopilot/tools/optimizer/pitch_roll_opt.py
@@ -4,8 +4,9 @@ from scl import generate_map
 from opcd_interface import OPCD_Interface
 from msgpack import loads
 import sys
-from time import sleep
 from copy import copy
+from time import sleep
+
 
 MUTATION_RATE = 0.1
 NUM_SAMPLES = 1000
@@ -16,7 +17,7 @@ debug = gates['blackbox']
 
 
 param_names = ['p', 'i', 'd']
-prefix = 'autopilot.controllers.u_pos.'
+prefix = 'autopilot.controllers.attitude.'
 vec = [ opcd.get(prefix + n) for n in param_names]
 fit_best = sys.float_info.max
 vec_best = copy(vec)
@@ -25,14 +26,14 @@ while True:
    vec = map(lambda x: x * uniform(1.0 - MUTATION_RATE, 1.0 + MUTATION_RATE), vec)
    
    # send new gains to opcd:
-   for i, n in zip(range(3), param_names):
+   for i, n in zip(range(4), param_names):
       opcd.set(prefix + n, vec[i])
    
    # read data from autopilot and compute fitness:
    fit = 0.0
    for _ in range(NUM_SAMPLES):
       array = loads(debug.recv())
-      fit += array[28] ** 2
+      fit += array[35] ** 2 + array[36] ** 2
    fit /= NUM_SAMPLES
    
    if fit < fit_best:
