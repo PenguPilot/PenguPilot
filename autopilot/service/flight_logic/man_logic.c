9a8aa9a8552d35c46ea6fb03060361d3a37f05f8
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-06-18 20:55:06 +0200

    switched to simpler ahrs

--- autopilot/service/flight_logic/man_logic.c
+++ autopilot/service/flight_logic/man_logic.c
@@ -327,6 +327,7 @@ bool man_logic_run(bool *hard_off, uint16_t sensor_status, bool flying, float ch
       {
          set_horizontal_spd_or_pos(pitch, roll, yaw, ne_gps_pos, ultra_u_pos);
          set_vertical_spd_or_pos(gas_stick - 0.5, baro_u_pos, ultra_u_pos, dt);
+         //cm_u_set_acc(tsfloat_get(&gas_acc_max) * (gas_stick - 0.5));
          break;
       }
    }
