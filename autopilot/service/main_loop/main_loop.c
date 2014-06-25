5b57b9993e746b57ba87a924e111560432e4b5f0
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-06-25 16:08:44 +0200

    improved comments in yaml file

--- autopilot/service/main_loop/main_loop.c
+++ autopilot/service/main_loop/main_loop.c
@@ -481,11 +481,13 @@ void main_step(const float dt,
       FOR_N(i, platform.n_motors) setpoints[i] = platform.ac.off_val;
    
    
-   printf("%f %f %f %f %f %f %f %f %f %f %f %f\n", euler.pitch, euler.roll, euler.yaw,
+   printf("%f %f %f %f %f %f %f %f %f %f %f %f %f\n", euler.pitch, euler.roll, euler.yaw,
                       pos_est.ne_pos.x, pos_est.ne_speed.x,
                       pos_est.ne_pos.y, pos_est.ne_speed.y,
                       pos_est.ultra_u.pos, pos_est.ultra_u.speed,
-                      voltage, current, (hard_off || !motors_enabled) ? 0 : thrust);
+                      voltage, current, (hard_off || !motors_enabled) ? 0 : thrust, baro);
+   
+   
    /* write motors: */
    if (!override_hw)
    {
