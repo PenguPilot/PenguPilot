021b11244f6ce63e85111e0c1107d007ec56ad9e
TobiasSimon <tobias.simon@tu-ilmenau.de>
2014-08-13 19:47:23 +0200

    added logger service

---
+++ logger/service/logger.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+  
+ Logger Service
+
+ Copyright (C) 2014 Tobias Simon, Ilmenau University of Technology
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+from os import sep
+from scl import generate_map
+from misc import daemonize, user_data_dir
+
+
+def main(name):
+   socket = generate_map(name)['log_data']
+   prefix = user_data_dir + sep + 'log' + sep
+   try:
+      new_file = prefix + 'session.log'
+      f = open(new_file, "wb")
+      while True:
+         data = socket.recv()
+         f.write(data)
+         f.flush()
+   finally:
+      try:
+         f.close()
+      except:
+         pass
+
+
+daemonize('logger', main)
+
