d75380c0c67a89183e5e7e74d818ebc2386ae103
Jan Roemisch <jan.roemisch@tu-ilmenau.de>
2015-05-12 17:12:01 +0200

    added forces test program

---
+++ motors/tools/forces_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+  
+ OMAP3-PWM Motor Test Program
+
+ Copyright (C) 2014 Tobias Simon
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+
+from time import sleep
+from sys import argv
+from scl import scl_get_socket
+from msgpack import dumps
+
+socket = scl_get_socket('motor_forces', 'pub')
+
+def write_motors(val):
+   forces = [val, val, val, val]
+   socket.send(dumps(forces))
+
+
+while 1:
+   write_motors(0.0)
+   sleep(0.005)
+
