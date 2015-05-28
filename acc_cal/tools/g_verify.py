4188b6e9d95358ec57133b3bb74693148fdb2c4e
TobiasSimon <tobias.simon@tu-ilmenau.de>
2015-05-28 20:53:36 +0200

    implemented g-verify

--- acc_cal/tools/g_verify.py
+++ acc_cal/tools/g_verify.py
@@ -28,33 +28,14 @@
 
 from scl import scl_get_socket
 from msgpack import loads
+from numpy.linalg import norm
+
 
 socket = scl_get_socket('acc_cal', 'sub')
 while True:
    try:
       vec = loads(socket.recv())
-      points.append(vec)
-      print len(points)
+      print 'acc g vector magnitude:', norm(vec)
    except:
       break
-
-c = Calibration(points)
-cal = c.get_cal()
-names = ['acc_bias_x', 'acc_bias_y', 'acc_bias_z', 'acc_scale_x', 'acc_scale_y', 'acc_scale_z']
-
-# detect calibration failure:
-fail = False
-for i in range(len(names)):
-   val = float(cal[i])
-   if isnan(val):
-      print 'bad calibration'
-      fail = True
-      break
-
-# write opcd values:
-if not fail:
-   for i in range(len(names)):
-      val = float(cal[i])
-      opcd.set('acc_adc_cal.' + names[i], val)
-   print 'accelerometer calibration complete, please type persist() in pp_opcd_shell to save'
 
