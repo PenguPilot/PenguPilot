b6a0fe2c0ad51c82749f700f4dbfc29cd752f975
Tobias Simon <tobias.simon@tu-ilmenau.de>
2015-07-18 12:51:06 +0200

    Update gps_print.py

--- gpsp/tools/gps_print.py
+++ gpsp/tools/gps_print.py
@@ -1,11 +1,10 @@
 from scl import scl_get_socket
-from msgpack import loads
 from gps_msgpack import *
 
 
 s = scl_get_socket('gps', 'sub')
 while True:
-   gps_data = loads(s.recv())
+   gps_data = s.recv()
    f = fix(gps_data)
    if f >= 2:
       print gps_data[LAT], gps_data[LON]
