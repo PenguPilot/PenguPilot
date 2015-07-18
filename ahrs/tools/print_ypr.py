d14d822ccaa2254b8bcc7df142e158b52924edbd
Tobias Simon <tobias.simon@tu-ilmenau.de>
2015-07-18 12:19:37 +0200

    Update print_ypr.py

--- ahrs/tools/print_ypr.py
+++ ahrs/tools/print_ypr.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+  
+ Orientation Output Utility
+ Copyright (C) 2015 Tobias Simon, Integrated Communication Systems Group, TU Ilmenau
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
 from scl import scl_get_socket
 from geomath import rad2deg
 
