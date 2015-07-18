63efc9c7354e63c4dd3df1ebea5143be8245619e
Tobias Simon <tobias.simon@tu-ilmenau.de>
2015-07-18 12:50:18 +0200

    Update print_sat_signals.py

--- gpsp/tools/print_sat_signals.py
+++ gpsp/tools/print_sat_signals.py
@@ -1,11 +1,9 @@
 from scl import scl_get_socket
-from msgpack import loads
 
 s = scl_get_socket('sats', 'sub')
 while True:
-   d = loads(s.recv())
    sum = 0.0
-   for x in d:
+   for x in s.recv():
       sum += x[4]
       print x[4],
    sum /= len(d)
