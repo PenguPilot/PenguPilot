f3737c0c146835a05ad0e40aa0d7c9c52fdefcb0
Tobias Simon <TobiasSimon@users.noreply.github.com>
2015-07-22 22:28:08 +0200

    Update ap_test.py

--- autopilot/tools/ap_test.py
+++ autopilot/tools/ap_test.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+ 
+ Autopilot Test Program
+ 
+ Copyright (C) 2015 Tobias Simon
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
 from scl import scl_get_socket
 
 ctrl = scl_get_socket('ap_ctrl', 'req')
