1bc724ff6930f55332648c833c878c66d67b2ca2
Oleksandr Andryeyev <ghostplus@gmail.com>
2015-10-19 21:48:49 +0100

    Create ap_test2.py

---
+++ autopilot/tools/ap_test2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+  ___________________________________________________
+ |  _____                       _____ _ _       _    |
+ | |  __ \                     |  __ (_) |     | |   |
+ | | |__) |__ _ __   __ _ _   _| |__) || | ___ | |_  |
+ | |  ___/ _ \ '_ \ / _` | | | |  ___/ | |/ _ \| __| |
+ | | |  |  __/ | | | (_| | |_| | |   | | | (_) | |_  |
+ | |_|   \___|_| |_|\__, |\__,_|_|   |_|_|\___/ \__| |
+ |                   __/ |                           |
+ |  GNU/Linux based |___/  Multi-Rotor UAV Autopilot |
+ |___________________________________________________|
+ 
+ Autopilot Test Program
+ 
+ Copyright (C) 2015 Tobias Simon
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details. """
+
+
+from scl import scl_get_socket
+from time import sleep
+
+ctrl = scl_get_socket('ap_ctrl', 'req')
+state = scl_get_socket('ap_state', 'sub')
+
+cmds = [
+'takeoff',
+'land'
+]
+
+try:
+    for cmd in cmds:
+        ctrl.send(cmd)
+        rep = ctrl.recv()
+        if not rep[0]:
+            print rep[1]
+        while state.recv() not in ['hovering', 'standing']:
+            pass
+        sleep(10)
+    print "Test finished."
+except KeyboardInterrupt:
+    #quit
+    print "Interrupting autopilot"
+    ctrl.send('kill')
