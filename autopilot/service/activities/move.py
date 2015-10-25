d83893a023897f4e89238b1748acd33edf74f028
Oleksandr Andryeyev <ghostplus@gmail.com>
2015-10-25 14:59:50 +0000

    Fixed move and added exit on timeout

--- autopilot/service/activities/move.py
+++ autopilot/service/activities/move.py
@@ -26,10 +26,11 @@
 
 
 from math import hypot
-from time import sleep
+from time import sleep, time
 from geomath import LinearInterpolation, gps_add_meters, gps_meters_offset
 from numpy import array, zeros
 from activity import Activity, StabMixIn
+from pylogger import *
 
 class MoveActivity(Activity, StabMixIn):
 
@@ -41,10 +42,24 @@ class MoveActivity(Activity, StabMixIn):
    def run(self):
       type, x, y = self.autopilot.arg
       ap = self.autopilot
+      timer_timeout = 20
+      
+      hp_err = SCL_Reader('hp_ctrl_err', 'sub')
+      sleep(0.1)
+      
       if type == 'spr': # starting point relative
          ap.start_pos = [ap.pse.data[4], ap.pse.data[6]]
-         ap.api.set_hp([x - ap.start_pos[0], y - ap.start_pos[1]])
- 
+         ap.api.set_hp([ap.start_pos[0] + x, ap.start_pos[1] + y])
+      
+      sleep(0.1)
+      
+      start_time = time()
+      timer = 0
+      while ( abs(err.data[0]) > 0.15 or abs(err.data[1]) > 0.15) and timer < timer_timeout:
+        timer = time - start_time
+      if timer > timer_timeout:
+        log(LL_INFO, "move timeout")
+      ap.api.set_hp([ap.pse.data[4], ap.pse.data[6])
       #self.stabilize()
       
       if not self.canceled:
